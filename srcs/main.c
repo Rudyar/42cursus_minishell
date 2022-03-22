3f23f49cfea7b49075a2bec2eec74d74d58d68cf
arudy <arthur.rudy.code@gmail.com>
2022-03-22 11:44:20 +0100

    Parsing : env_path...

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:19:49 by arudy             #+#    #+#             */
-/*   Updated: 2022/03/22 11:41:03 by arudy            ###   ########.fr       */
+/*   Updated: 2022/03/22 11:43:34 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 int	main(int ac, char **av, char **envp)
 {
 	char	*line;
-	char	**full_path;
+	char	**env_path;
 
 	(void)av;
 	if (ac != 1)
 		return (0);
-	full_path = get_full_path(envp);
+	env_path = get_full_path(envp);
 	// while (1)
 	// {
 		line = readline("Minishell : ");
 		printf("%s\n", line);
 		free(line);
 	// }
-	free_strs(full_path);
+	free_strs(env_path);
 	return (0);
 }
