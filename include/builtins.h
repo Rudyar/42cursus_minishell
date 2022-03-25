9f503a4093935e9687ef01aa131642ecb4c0b509
louislvq <louis.leveque@outlook.com>
2022-03-25 16:26:28 +0100

    BUILTINS : env updated

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:22:46 by arudy             #+#    #+#             */
-/*   Updated: 2022/03/24 19:27:10 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/03/25 15:53:01 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int	echo_cmd(char **args);
 int	cd_cmd(char **args);
-int	env_cmd(t_env *env);
+int	env_cmd(char **args, t_env *env);
 int	export_cmd(char **args, char ***envp);
 int	pwd_cmd(void);
 
--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:19:49 by arudy             #+#    #+#             */
-/*   Updated: 2022/03/25 09:48:57 by arudy            ###   ########.fr       */
+/*   Updated: 2022/03/25 16:25:58 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 
 int	main(int ac, char **av, char **envp)
 {
-	char	*line;
+	// char	*line;
 	t_env	*env;
-	t_token	*token;
+	// t_token	*token;
 
 	(void)av;
-	if (ac != 1)
-		return (0);
+	(void)ac;
+	// if (ac != 1)
+		// return (0);
 	env = init_env(envp);
-	while (1)
-	{
-		line = readline("Minishell : ");
-		if (line)
-		{
-			lexer(&token, line);
-			free(line);
-		}
-	}
+	// while (1)
+	// {
+	// 	line = readline("Minishell : ");
+		// if (line)
+		// {
+		// 	lexer(&token, line);
+		// 	free(line);
+		// }
+	// }
 	ft_free(env);
 	return (0);
 }
