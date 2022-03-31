b095924c2a358b8e37f92ca52031b08c4f88e37e
lleveque42 <louis.leveque@outlook.com>
2022-03-31 19:16:22 +0200

    PUSH POUR TEST

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,22 +6,26 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 16:26:20 by lleveque          #+#    #+#             */
-/*   Updated: 2022/03/30 16:58:40 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/03/31 19:10:08 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../../include/minishell.h"
+#include "../../include/minishell.h"
 
-// int	env_cmd(t_env *env)
-// {
-// 	int	i;
+int	env_cmd(t_env *env)
+{
+	int	i;
 
-// 	i = 1;
-// 	while (env)
-// 	{
-// 		ft_putstr_fd(env->var, 1);
-// 		ft_putstr_fd("\n", 1);
-// 		env = env->next;
-// 	}
-// 	return (0);
-// }
+	while (env)
+	{
+		i = 0;
+		while (env->var[i])
+		{
+			ft_putstr_fd(env->var[i], 1);
+			++i;
+		}
+		ft_putstr_fd("\n", 1);
+		env = env->next;
+	}
+	return (0);
+}
--- srcs/main.c
+++ srcs/main.c
@@ -6,48 +6,48 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:19:49 by arudy             #+#    #+#             */
-/*   Updated: 2022/03/30 16:37:37 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/03/31 19:10:20 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// int	main(int ac, char **av, char **envp)
-// {
-// 	t_env	*env;
-
-// 	(void)av;
-// 	(void)ac;
-// 	env = init_env(envp);
-// 	// export_cmd(av, env);
-// 	unset_cmd(av, env);
-// 	env_cmd(env);
-// 	ft_free(env);
-// 	return (0);
-// }
-
 int	main(int ac, char **av, char **envp)
 {
-	char	*line;
 	t_data	*data;
 
 	(void)av;
 	(void)ac;
 	data = init_data(envp);
-	while (1)
-	{
-		line = readline("Minishell : ");
-		if (*line)
-		{
-			if (parsing(line, data) == 0)
-			{
-				// exec
-				// print_token_lst(&data->tokens);
-				free_token_lst(&data->tokens);
-			}
-			free(line);
-		}
-	}
+	// export_cmd(av, env);
+	// unset_cmd(av, env);
+	// env_cmd(data->env);
 	ft_free(data);
 	return (0);
 }
+
+// int	main(int ac, char **av, char **envp)
+// {
+// 	char	*line;
+// 	t_data	*data;
+
+// 	(void)av;
+// 	(void)ac;
+// 	data = init_data(envp);
+// 	while (1)
+// 	{
+// 		line = readline("Minishell : ");
+// 		if (*line)
+// 		{
+// 			if (parsing(line, data) == 0)
+// 			{
+// 				// exec
+// 				// print_token_lst(&data->tokens);
+// 				free_token_lst(&data->tokens);
+// 			}
+// 			free(line);
+// 		}
+// 	}
+// 	ft_free(data);
+// 	return (0);
+// }
