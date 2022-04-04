c7813b88742402e0a40adc2fc15a3d76bb23b593
louislvq <louis.leveque@outlook.com>
2022-04-04 17:33:35 +0200

    LOL : printf deleted

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 18:57:22 by lleveque          #+#    #+#             */
-/*   Updated: 2022/04/04 17:09:02 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/04/04 17:33:00 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	free_env(t_env *lst)
 	{
 		i = 0;
 		tmp = lst;
-		printf("lst.var[0] = %s\n", lst->var[0]);
 		while (lst->var[i])
 		{
 			free(lst->var[i]);
