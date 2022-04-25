848debfa4f994e3c34f643bd4aec36078b4a5ea2
louislvq <louis.leveque@outlook.com>
2022-04-25 19:13:47 +0200

    EXEC : close launch_ugo updated

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -3,47 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 14:58:13 by arudy             #+#    #+#             */
-/*   Updated: 2022/04/25 15:36:31 by arudy            ###   ########.fr       */
+/*   Updated: 2022/04/25 19:12:19 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-	// if (!lst->prev && lst->in != 0)
-	// {
-	// 	if (dup2(lst->in,  STDIN_FILENO) == -1)
-	// 		exec_error("Dup2 error", lst, data);
-	// }
-	// else if (lst->prev && lst->in == 0)
-	// {
-	// 	close(lst->prev->pipe[1]);
-	// 	if (dup2(lst->prev->pipe[0], STDIN_FILENO) == -1)
-	// 		exec_error("Dup2 error", lst, data);
-	// }
-	// else if (lst->prev && lst->in != 0)
-	// {
-	// 	if (dup2(lst->in,  STDIN_FILENO) == -1)
-	// 		exec_error("Dup2 error", lst, data);
-	// }
-	// if (lst->next && lst->out == 1)
-	// {
-	// 	close(lst->pipe[0]);
-	// 	if (dup2(lst->pipe[1], STDOUT_FILENO) == -1)
-	// 		exec_error("Dup2 error", lst, data);
-	// }
-	// else if (lst->next && lst->out != 1)
-	// {
-	// 	if (dup2(lst->out, STDOUT_FILENO) == -1 )
-	// 		exec_error("Dup2 error", lst, data);
-	// }
-	// else if (!lst->next && lst->out != 1)
-	// {
-	// 	if (dup2(lst->out, STDOUT_FILENO) == -1)
-	// 		exec_error("Dup2 error", lst, data);
-	// }
 
 static void	exec_cmd(t_cmd *lst, t_data *data)
 {
@@ -68,13 +35,12 @@ static void	launch_ugo(t_cmd *lst, t_data *data)
 	if (lst->out > 2)
 		if (dup2(lst->out, STDOUT_FILENO) == -1)
 			exec_error("Dup2 error 2", lst, data);
-	if (lst->next)
+	if (lst->next && lst->next->in == lst->pipe[0])
 		close(lst->pipe[0]);
+	if (lst->prev && lst->prev->out == lst->prev->pipe[1] \
+		&& lst->in == lst->prev->pipe[0])
+		close(lst->prev->pipe[1]);
 	exec_cmd(lst, data);
-	if (lst->in > 2)
-		close(lst->in);
-	if (lst->out > 2)
-		close(lst->out);
 }
 
 static void	wait_fork(t_cmd *lst, t_data *data)
@@ -104,6 +70,10 @@ void	start_exec(t_cmd *lst, t_data *data)
 			exec_error("Fork error", lst, data);
 		if (lst->fork == 0)
 			launch_ugo(lst, data);
+		if (lst->in > 2)
+			close(lst->in);
+		if (lst->out > 2)
+			close(lst->out);
 		lst = lst->next;
 	}
 	wait_fork(head_lst, data);
--- srcs/exec/exec_utils.c
+++ srcs/exec/exec_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 14:03:42 by arudy             #+#    #+#             */
-/*   Updated: 2022/04/25 14:29:25 by arudy            ###   ########.fr       */
+/*   Updated: 2022/04/25 16:19:09 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ extern int	g_exit_status;
 
 int	check_builtins(t_cmd *lst, t_data *data)
 {
-	if (ft_strncmp(lst->cmd[0], "cd", 3) == 0)
+	if (!ft_strncmp(lst->cmd[0], "cd", 3))
 		return (cd_cmd(lst->cmd, data), 1);
-	else if (ft_strncmp(lst->cmd[0], "pwd", 4) == 0)
+	else if (!ft_strncmp(lst->cmd[0], "pwd", 4))
 		return (pwd_cmd(), 1);
-	else if (ft_strncmp(lst->cmd[0], "echo", 5) == 0)
+	else if (!ft_strncmp(lst->cmd[0], "echo", 5))
 		return (echo_cmd(lst->cmd), 1);
-	else if (ft_strncmp(lst->cmd[0], "env", 4) == 0)
+	else if (!ft_strncmp(lst->cmd[0], "env", 4))
 		return (env_cmd(data), 1);
-	else if (ft_strncmp(lst->cmd[0], "export", 7) == 0)
+	else if (!ft_strncmp(lst->cmd[0], "export", 7))
 		return (export_cmd(lst->cmd, data), 1);
-	else if (ft_strncmp(lst->cmd[0], "unset", 6) == 0)
+	else if (!ft_strncmp(lst->cmd[0], "unset", 6))
 		return (unset_cmd(lst->cmd, data), 1);
-	else if (ft_strncmp(lst->cmd[0], "exit", 6) == 0)
+	else if (!ft_strncmp(lst->cmd[0], "exit", 6))
 		return (exit_cmd(lst->cmd, data), 1);
 	return (0);
 }
