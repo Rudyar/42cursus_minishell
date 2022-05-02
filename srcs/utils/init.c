d7a1794c5b4a965bcd537bb2f64fccc6bce4364c
lleveque42 <louis.leveque@outlook.com>
2022-05-02 16:49:42 +0200

    INIT : mini env is env -i && SHLVL++

--- srcs/utils/init.c
+++ srcs/utils/init.c
@@ -3,16 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 18:22:42 by lleveque          #+#    #+#             */
-/*   Updated: 2022/04/19 16:46:11 by arudy            ###   ########.fr       */
+/*   Updated: 2022/05/02 16:47:22 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
 extern int	g_exit_status;
+
+t_env	*mini_env(t_data *data)
+{
+	t_env	*env;
+	char	*tmp;
+
+	env = ft_env_lstnew("PWD=", NULL, data);
+	tmp = getcwd(NULL, 0);
+	set_env(env, data, "PWD", ft_strdup(tmp, data));
+	free(tmp);
+	env->next = ft_env_lstnew("SHLVL=", NULL, data);
+	set_env(env, data, "SHLVL", ft_strdup("1", data));
+	env->next->next = ft_env_lstnew("_=", NULL, data);
+	set_env(env, data, "_", ft_strdup("./minishell", data));
+	return (env);
+}
+
+void	update_shlvl(t_env *env, t_data *data)
+{
+	int	tmp;
+
+	while (env)
+	{
+		if (!ft_strncmp(env->var[0], "SHLVL", 5))
+		{
+			if (!env->var[1])
+			{
+				env->var[1] = ft_strdup("1", data);
+				break;
+			}
+			tmp = ft_atoi(env->var[1]);
+			tmp++;
+			ft_free(env->var[1], data);
+			env->var[1] = ft_itoa(tmp, data);
+		}
+		env = env->next;
+	}
+}
 
 t_env	*init_env(char **envp, t_data *data)
 {
@@ -22,7 +60,7 @@ t_env	*init_env(char **envp, t_data *data)
 
 	i = 1;
 	if (!envp[0])
-		return (NULL);
+		return (mini_env(data));
 	env = ft_env_lstnew(envp[0], NULL, data);
 	if (envp[i])
 	{
@@ -38,8 +76,8 @@ t_env	*init_env(char **envp, t_data *data)
 				tmp->next = NULL;
 		}
 	}
-	else
-		env->next = NULL;
+	if (ft_getenv(env, "SHLVL"))
+		update_shlvl(env, data);
 	return (env);
 }
 
