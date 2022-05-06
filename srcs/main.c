abf7797cc1ab44ed94afcfde5f0bbd9152953ef5
louislvq <louis.leveque@outlook.com>
2022-05-06 20:45:44 +0200

    MAIN : tmp signal handling

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:19:49 by arudy             #+#    #+#             */
-/*   Updated: 2022/05/02 17:04:06 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/05/06 20:31:43 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	main(int ac, char **av, char **envp)
 
 	(void)av;
 	(void)ac;
+	// signal(SIGINT, SIG_IGN);
+	// signal(SIGQUIT, SIG_IGN);
 	g_exit_status = 0;
 	data = init_data(envp);
 	loop(data, envp);
